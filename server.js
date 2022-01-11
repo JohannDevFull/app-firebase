438f7b8a41a215787a30388b99081b1dfaea509a
Johann Ramirez <jramirez@wolke.com.co>
2022-01-10 19:04:53 -0500

    package update and serve.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-myagenda'));
+app.use(express.static('./dist/angular-myagenda/'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-myagenda/'}),
